008971860a0efe0731f0a4549c8ba14f4113fe0f
Esteban Palacio <estipalacio@gmail.com>
2021-11-13 13:48:39 -0500

    added styling for multiple components

--- src/components/EventList.component.jsx
+++ src/components/EventList.component.jsx
@@ -1,13 +1,13 @@
-import { Fragment } from 'react';
+import styles from './EventList.module.css'
 
 export default function EventList({ events, handleClick }) {
     return (
         <div>
             {events.map((event) => (
-                <Fragment key={event.id}>
+                <div className={styles.card} key={event.id}>
                 <h2>{event.title}</h2>
                 <button onClick={() => handleClick(event.id)}>delete event</button>
-                </Fragment>
+                </div>
             ))}
         </div>
     )
